d4f42df085903b1b2b74b8a91e87abdf127d0f32
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2024-01-14 19:02:19 +0300

    Test

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -16,11 +16,14 @@ describe("Course List Row component test", () => {
     expect(wrapper.find("tr").childAt(0).html()).toEqual('<th style="background-color:#deb5b545" colSpan="2">test</th>');
   });
 
-  it("should render two cells when textSecondCell not null", () => {
-    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />);
-
-    expect(wrapper.find("tr").children()).toHaveLength(2);
-    expect(wrapper.find("tr").childAt(0).html()).toEqual("<td>test</td>");
-    expect(wrapper.find("tr").childAt(1).html()).toEqual("<td>test</td>");
+  it("should render one cell with colspan = 2 when textSecondCell is null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />);
+  
+    expect(wrapper.find("th").props()).toEqual({
+      style: { backgroundColor: "#deb5b545" },
+      colSpan: 2,
+      children: "test",
+    });
   });
+  
 });
